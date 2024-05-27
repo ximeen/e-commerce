74a9e335b32fd69e8f96951a78c9dda03dd72618
Gabriel Ximenes <swg-ximenes@iMac-Pro-de-Gabriel.local>
2024-05-27 19:12:23 -0300

    :package: landing page completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/css/globals.css";
 import { NavBar } from "@/components/NavBar";
 import { ReactNode } from "react";
 import { Footer } from "@/components/Footer";
